689e04e4778ec8b77bb75a7c783ce3f99c17555d
CPDEZ08 <CPDEZ08@outlook.com>
2020-08-20 17:27:09 +0000

    New file added

---
+++ personal.py
@@ -0,0 +1,10 @@
+name = input("Enter your Name")
+age = int(input("Enter your age!"))
+print(name)
+print(type(name))
+print(age)
+print(type(age))
+print(name + " is " + str(age) + " years old")
+
+print(f"{name} is {age} years old")
+"Carlos Pires is 53 years old"
